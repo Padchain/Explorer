df24ffe4a2304bf87876604c2c5ea2d0578783dc
Matthew Reichardt <me@mattthewreichardt.com>
2016-01-05 23:01:37 -0600

    First working explorer.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  require 'json'
+  require 'logger'
+
+  def index
+    @info = BitcoinRpc.new('http://multichainrpc:9H7jWuTVXN9o158B9GrrTetfiSWeUVizdY4cRwHVw6uv@us-east.padchain.com:6306/').getinfo
+    @circulation = BitcoinRpc.new('http://multichainrpc:9H7jWuTVXN9o158B9GrrTetfiSWeUVizdY4cRwHVw6uv@us-east.padchain.com:6306/').getbalance
+    @assets = BitcoinRpc.new('http://multichainrpc:9H7jWuTVXN9o158B9GrrTetfiSWeUVizdY4cRwHVw6uv@us-east.padchain.com:6306/').listassets
+
+    block = @info['blocks']
+
+    block_count = block.to_i - 9
+
+    puts block_count
+
+    @current_block = []
+
+    x = 0
+
+    10.times do
+      block_hash = BitcoinRpc.new('http://multichainrpc:9H7jWuTVXN9o158B9GrrTetfiSWeUVizdY4cRwHVw6uv@us-east.padchain.com:6306/').getblockhash(block_count)
+      @current_block[x] = BitcoinRpc.new('http://multichainrpc:9H7jWuTVXN9o158B9GrrTetfiSWeUVizdY4cRwHVw6uv@us-east.padchain.com:6306/').getblock(block_hash)
+      x = x + 1
+      block_count = block_count + 1
+    end
+
+  end
+
+end
