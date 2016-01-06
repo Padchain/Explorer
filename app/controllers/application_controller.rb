93e15c0cd5f2d5d0b6124137b28ec0427dd931c1
Matthew Reichardt <me@mattthewreichardt.com>
2016-01-06 00:27:50 -0600

    Fixed whitelisted location.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  config.web_console.whitelisted_ips = '75.132.235.72'
 end
