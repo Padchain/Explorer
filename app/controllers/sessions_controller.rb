9b66925d9ab2762179b417dcd4c7b6b8394ac130
Matthew Reichardt <me@mattthewreichardt.com>
2016-01-13 11:28:39 -0600

    Brand new front-end design!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,15 @@ class SessionsController < ApplicationController
     @circulation = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getbalance
     @assets = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').listassets
     @network_hash = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getnetworkhashps / 1000
+    @unconfirmed_transactions = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getmempoolinfo
+
+    if BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getinfo['blocks'] = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getblockchaininfo['blocks']
+      @sync_status = "synched"
+      @sync_text = "IN SYNC"
+    else
+      @sync_status = "behind"
+      @sync_text = "BEHIND"
+    end
 
     block = @info['blocks']
 
@@ -16,14 +25,20 @@ class SessionsController < ApplicationController
     puts block_count
 
     @current_block = []
-
     x = 0
+    @tx_count = 0
 
-    10.times do
+    5.times do
       block_hash = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getblockhash(block_count)
       @current_block[x] = BitcoinRpc.new('http://multichainrpc:' + ENV['PADCHAIN_NODE_KEY'] + '@' + ENV['PADCHAIN_NODE_ADDRESS'] + ':6306/').getblock(block_hash)
       x = x + 1
       block_count = block_count + 1
+    end
+
+    @current_block.reverse.each do |c|
+      c['tx'].each do |t|
+        @tx_count = @tx_count + 1
+      end
     end
 
   end
